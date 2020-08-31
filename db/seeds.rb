f30c7d25c40a6783496cc6da7a4085f59a3103be
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-31 14:22:31 +0300

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course = Course.create(title: 'Hello World', description: 'Create a react app with ruby on rails')
+
+section = Section.create(title: 'Chapter 1', course: course)
+
+episodes = Episode.create([
+    { title: '1. Setting up a new Ruby on Rails app with React', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+    { title: '2. Adding React to an existing Rails app', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+    { title: '3. Building a hello world app', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+    { title: '4. Adding react router dom to your App', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+])
